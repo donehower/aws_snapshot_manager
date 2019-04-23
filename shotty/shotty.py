0c1a4155defeabce5fecca85a5bf422e55c7f5d7
Sarah Donehower <sarahdonehower@gmail.com>
2019-04-23 14:57:06 -0700

    Added info returned by call

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,13 +1,26 @@
 import boto3
-
+import click
 
 session = boto3.Session(profile_name='shotty')
 ec2 = session.resource('ec2')
 
 
+@click.command()
 def list_instances():
+    '''
+    Prints active, running EC2 instances to the console.
+    For each instance prints: id, type, AZ, state, dns name.
+    '''
     for i in ec2.instances.all():
-        print(i)
+        print(', '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name
+        )))
+
+    return
 
 
 if __name__ == '__main__':
