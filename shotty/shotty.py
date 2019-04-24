bbef56786fd29f2daca2ae84dbbf789669d6e7c5
Sarah Donehower <sarahdonehower@gmail.com>
2019-04-24 09:56:28 -0700

    start/stop instance around taking snapshot

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -90,10 +90,19 @@ def create_snapshots(project):
     instances = filter_instances(project)
 
     for i in instances:
+        print("Stopping instance {0}...".format(i.id))
         i.stop()
+        i.wait_until_stopped()
+
         for v in i.volumes.all():
             print("Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by snapshot-manager")
+
+        print("Starting instance {0}...\n".format(i.id))
+        i.start()
+        i.wait_until_running()
+
+    print("Snapshots completed.")
 
     return
 
